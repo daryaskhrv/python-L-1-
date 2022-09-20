c4f8f11f1eefbdf8db578b1ddec31e203fe2243b
Darya Sakharova <daryasheqer@yandex.ru>
2022-09-20 22:25:18 +0400

    Промежуточный результат, написание нужных функций

--- Lab1/sourse.py
+++ Lab1/sourse.py
@@ -13,5 +13,53 @@
 #После загрузки всех изображений, необходимо их просмотреть на соответствие классу. 
 #В случае замеченных несоответствий необходимо будет дополнить набор данных до минимального размера. 
 #Для избежания подобных ситуаций рекомендуется загружать изображения с запасом.
-import os, sys, requests
+import os, sys, requests, cv2
+from urllib import request
+from re import search
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 
+#https://yandex.ru/images/search?text=zebra
+search = {'text' : 'zebra'}
+URL = "https://yandex.ru/images/" 
+html_page = requests.get(URL + "search", params=search)
+# html_page.text - хранит html код веб-страницы
+html = BeautifulSoup(html_page.content, "html.parser")
+urls = []
+
+def is_valid(url):
+     #является ли url допустимым URL
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+def get_all_images(url):
+    for img in html.find_all("img"):
+        img_url = img.attrs.get("src")
+        if not img_url:
+            # если img не содержит атрибута src, просто пропустите
+            continue
+        #img_url = urljoin(url, img_url) 
+        #сделать URL абсолютным, присоединив домен к только что извлеченному URL
+        if is_valid(img_url):
+                urls.append(img_url)
+    return urls
+
+i = 1
+def download(url, pathname):
+    global i
+    if not os.path.isdir(pathname):
+        os.mkdir(pathname)
+    request_img = requests.get(url)
+    save = open(pathname + "/"+ str(i) + ".jpg", "wb") # открытие потока с типом wb
+    i += 1
+    save.write(request_img.content) # запись в файл
+    save.close() # закрытие файлового потока
+    
+def main(url, path):
+    # получить все изображения
+    imgs = get_all_images(url)
+    for img in imgs:
+        # для каждого изображения, загрузите его
+        download(img, path)
+
+main("https://yandex.ru/images/search?text=zebra", "images")
